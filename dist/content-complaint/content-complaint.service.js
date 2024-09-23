a9efbd511e6e1dbd2224ba742745a232e64e9ddc
unknown <dimab787898@gmail.com>
2024-09-23 19:06:07 +0300

    content complaint

--- dist/content-complaint/content-complaint.service.js
+++ dist/content-complaint/content-complaint.service.js
@@ -5,13 +5,77 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.ContentComplaintService = void 0;
 const common_1 = require("@nestjs/common");
+const prisma_service_1 = require("../prisma/prisma.service");
 let ContentComplaintService = class ContentComplaintService {
+    constructor(prisma) {
+        this.prisma = prisma;
+    }
+    async createContentComplaint(data) {
+        return this.prisma.contentComplaint.create({
+            data,
+        });
+    }
+    async getContentComplaintById(id) {
+        const complaint = await this.prisma.contentComplaint.findUnique({
+            where: { id },
+        });
+        if (!complaint) {
+            throw new common_1.NotFoundException(`ContentComplaint with ID ${id} not found`);
+        }
+        return complaint;
+    }
+    async updateContentComplaint(id, data) {
+        const complaint = await this.prisma.contentComplaint.findUnique({
+            where: { id },
+        });
+        if (!complaint) {
+            throw new common_1.NotFoundException(`ContentComplaint with ID ${id} not found`);
+        }
+        return this.prisma.contentComplaint.update({
+            where: { id },
+            data,
+        });
+    }
+    async deleteContentComplaint(id) {
+        const complaint = await this.prisma.contentComplaint.findUnique({
+            where: { id },
+        });
+        if (!complaint) {
+            throw new common_1.NotFoundException(`ContentComplaint with ID ${id} not found`);
+        }
+        return this.prisma.contentComplaint.delete({
+            where: { id },
+        });
+    }
+    async getAllContentComplaints() {
+        return this.prisma.contentComplaint.findMany();
+    }
+    async countComplaintsByReasonId(reasonId) {
+        return this.prisma.contentComplaint.count({
+            where: { reasonId },
+        });
+    }
+    async resolveContentComplaint(id) {
+        return this.prisma.contentComplaint.update({
+            where: { id },
+            data: { status: 'Resolved', resolvedAt: new Date() },
+        });
+    }
+    async getComplaintsByUserId(userId) {
+        return this.prisma.contentComplaint.findMany({
+            where: { issuedBy: userId },
+        });
+    }
 };
 exports.ContentComplaintService = ContentComplaintService;
 exports.ContentComplaintService = ContentComplaintService = __decorate([
-    (0, common_1.Injectable)()
+    (0, common_1.Injectable)(),
+    __metadata("design:paramtypes", [prisma_service_1.PrismaService])
 ], ContentComplaintService);
 //# sourceMappingURL=content-complaint.service.js.map
--- src/content-complaint/content-complaint.controller.ts
+++ src/content-complaint/content-complaint.controller.ts
@@ -1,4 +1,73 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  Get,
+  Put,
+  Delete,
+  Param,
+  Body,
+} from '@nestjs/common';
+import { ContentComplaintService } from './content-complaint.service';
+import { ContentComplaint, Prisma } from '@prisma/client';
 
 @Controller('content-complaint')
-export class ContentComplaintController {}
+export class ContentComplaintController {
+  constructor(
+    private readonly contentComplaintService: ContentComplaintService,
+  ) {}
+
+  @Post()
+  async createContentComplaint(
+    @Body() data: Prisma.ContentComplaintCreateInput,
+  ): Promise<ContentComplaint> {
+    return this.contentComplaintService.createContentComplaint(data);
+  }
+
+  @Get(':id')
+  async getContentComplaintById(
+    @Param('id') id: number,
+  ): Promise<ContentComplaint> {
+    return this.contentComplaintService.getContentComplaintById(id);
+  }
+
+  @Put(':id')
+  async updateContentComplaint(
+    @Param('id') id: number,
+    @Body() data: Prisma.ContentComplaintUpdateInput,
+  ): Promise<ContentComplaint> {
+    return this.contentComplaintService.updateContentComplaint(id, data);
+  }
+
+  @Delete(':id')
+  async deleteContentComplaint(
+    @Param('id') id: number,
+  ): Promise<ContentComplaint> {
+    return this.contentComplaintService.deleteContentComplaint(id);
+  }
+
+  @Get()
+  async getAllContentComplaints(): Promise<ContentComplaint[]> {
+    return this.contentComplaintService.getAllContentComplaints();
+  }
+
+  @Get('count/reason/:reasonId')
+  async countComplaintsByReasonId(
+    @Param('reasonId') reasonId: number,
+  ): Promise<number> {
+    return this.contentComplaintService.countComplaintsByReasonId(reasonId);
+  }
+
+  @Put('resolve/:id')
+  async resolveContentComplaint(
+    @Param('id') id: number,
+  ): Promise<ContentComplaint> {
+    return this.contentComplaintService.resolveContentComplaint(id);
+  }
+
+  @Get('user/:userId')
+  async getComplaintsByUserId(
+    @Param('userId') userId: number,
+  ): Promise<ContentComplaint[]> {
+    return this.contentComplaintService.getComplaintsByUserId(userId);
+  }
+}
