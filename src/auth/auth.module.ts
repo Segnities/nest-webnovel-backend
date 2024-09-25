74cbd81ce950b0b0aa60dcc10e6fd74f927fa9bf
unknown <dimab787898@gmail.com>
2024-09-25 10:46:57 +0300

    firebase guard & decorator

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,11 +1,9 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import { FirebaseAuthStrategy } from './auth.strategy';
 import { PrismaService } from '@/prisma/prisma.service';
 
 @Module({
   imports: [ConfigModule],
-  providers: [FirebaseAuthStrategy, PrismaService],
-  exports: [FirebaseAuthStrategy],
+  providers: [PrismaService],
 })
 export class AuthModule {}
